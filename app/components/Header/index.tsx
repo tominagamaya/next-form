02f1d660ad31f8a7be0aa7f82bf34d5312bb596f
tominagamaya <maya.tominaga.9@gmail.com>
2024-03-03 21:32:12 +0900

    create header component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="mt-3 text-3xl font-extrabold tracking-tight text-slate-900">Next app</h1>
+    </div>
+  )
+}
